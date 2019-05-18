6c33fbd9f5e5507c6eacaeffe1b7e5d0dde0751f
Ashraful Robin <robinashraful71@gmail.com>
2019-05-18 12:53:19 +0600

    aa

--- app/src/main/java/com/inducesmile/androidpayexample/fragment/VideoFragment.java
+++ app/src/main/java/com/inducesmile/androidpayexample/fragment/VideoFragment.java
@@ -1,102 +1,79 @@
 package com.inducesmile.androidpayexample.fragment;
 
 
-import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.Toast;
+import android.widget.Button;
+import android.widget.ImageView;
 
+import com.google.android.youtube.player.YouTubeBaseActivity;
+import com.google.android.youtube.player.YouTubeInitializationResult;
+import com.google.android.youtube.player.YouTubePlayer;
+import com.google.android.youtube.player.YouTubePlayerView;
 import com.inducesmile.androidpayexample.OnBackPressed;
+import com.inducesmile.androidpayexample.PlayerConfig;
 import com.inducesmile.androidpayexample.R;
 import com.inducesmile.androidpayexample.ShoppingActivity;
-import com.inducesmile.androidpayexample.adapters.YoutubeRecyclerViewAdapter;
-import com.inducesmile.androidpayexample.model.products_model.Datum;
-import com.inducesmile.androidpayexample.model.products_model.Products;
-import com.inducesmile.androidpayexample.web_api.IClientServer;
-import com.inducesmile.androidpayexample.web_api.RetrofitService;
-
-import java.util.List;
-
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class VideoFragment extends Fragment implements OnBackPressed {
-    IClientServer iClientServer;
-    ProgressDialog progressDialog;
-    private RecyclerView recyclerView;
 
 
-    public VideoFragment() {
-        // Required empty public constructor
-    }
+    YouTubePlayerView youTubePlayerView;
+    Button button;
+
+
+    YouTubePlayer.OnInitializedListener onInitializedListener;
+
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        progressDialog = new ProgressDialog(getContext());
-        progressDialog.setMessage("Loading..");
-        progressDialog.show();
-        // Inflate the layout for this fragment
-        View view = inflater.inflate(R.layout.fragment_video, container, false);
-        recyclerView = view.findViewById(R.id.recyclerview);
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.video_list_item);
 
-        iClientServer = RetrofitService.getRetrofitInstance().create(IClientServer.class);
-        getProductsFromApi();
-        return view;
-    }
+        /* button=(Button) getView().findViewById(R.id.button);
 
-    public void getProductsFromApi() {
-        String id = "Cutprice@987";
-        final Call<Products> productsCall = iClientServer.getALlProducts(id);
+        youTubePlayerView=(YouTubePlayerView) getView().findViewById(R.id.youtube_player_view);*/
 
-        productsCall.enqueue(new Callback<Products>() {
+        onInitializedListener=new YouTubePlayer.OnInitializedListener() {
             @Override
-            public void onResponse(Call<Products> call, Response<Products> response) {
-                Products products = response.body();
-                progressDialog.dismiss();
-                loadDataList(products.getData());
+            public void onInitializationSuccess(YouTubePlayer.Provider provider, YouTubePlayer youTubePlayer, boolean b) {
+
+                youTubePlayer.loadVideo("LHeSaTQXiBk");
             }
 
             @Override
-            public void onFailure(Call<Products> call, Throwable t) {
-                Toast.makeText(getContext(), "Unable to load users " + t.getMessage(), Toast.LENGTH_SHORT).show();
+            public void onInitializationFailure(YouTubePlayer.Provider provider, YouTubeInitializationResult youTubeInitializationResult) {
+
+            }
+
+
+        };
+
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                youTubePlayerView.initialize(PlayerConfig.API_KEY,onInitializedListener);
             }
         });
+
+
     }
+
+
+
+    private void setContentView(int video_list_item) {
+    }
+
 
     @Override
     public void onBackPressed() {
         startActivity(new Intent(getActivity(), ShoppingActivity.class));
-    }
-
-    private void loadDataList(List<Datum> usersList) {
-
-//Get a reference to the RecyclerView//
-
-
-//Use a LinearLayoutManager with default vertical orientation//
-
-        YoutubeRecyclerViewAdapter shopAdapter = new YoutubeRecyclerViewAdapter(getContext(), usersList);
-        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
-        recyclerView.setAdapter(shopAdapter);
-        shopAdapter.notifyDataSetChanged();
-      /*  GridLayoutManager mGrid = new GridLayoutManager(getActivity(), 3);
-        shoppingRecyclerView.setLayoutManager(mGrid);
-        shoppingRecyclerView.setHasFixedSize(true);
-        shoppingRecyclerView.addItemDecoration(new SpacesItemDecoration(3, 12, false));*/
-
-
-//Set the Adapter to the RecyclerView//
-
     }
 }
